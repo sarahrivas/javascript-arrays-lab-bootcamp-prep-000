71bf81e96fd6075afb29f51a31d5caa7eabebd04
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-09 03:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ function prependKitten(name) {
   var newArray = [name, ...kittens];
   return newArray;
 }
+
+function removeLastKitten () {
+  return kittens.slice(0,-1);
+}
+
+function removeFirstKitten () {
+  return kittens.slice(1);
+}
